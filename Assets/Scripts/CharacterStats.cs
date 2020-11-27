b4207fb37a03e7c7f0ebc0d1ceaab4369947c549
Pablo Segura Gil <ppsgil21@gmail.com>
2020-11-27 18:29:33 +0100

    [Entrega 1]

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            print("La esfera ha muerto");
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            TakeDamage(10);
+        }
+       
+    }
+    
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public const float yDie = -30.0f;
+	
+	private static int bollsDestroyed = 0;
+	
+	private GameObject[] bolls;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+     	bolls = GameObject.FindGameObjectsWithTag("boll"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < yDie){
+			bollsDestroyed++;
+			Destroy(gameObject);
+			if(bollsDestroyed == bolls.Length){
+				Application.LoadLevel("FinalWin");	
+			}
+		}
+    }
+}
